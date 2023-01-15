5c05221c27685318e763d95d1aebe67f4ff33c98
kmolski <krzysztof.molski29@gmail.com>
2023-01-15 11:03:03 +0100

    Add test for fork protection inheritance and validation

--- user/vmprotect.c
+++ user/vmprotect.c
@@ -4,10 +4,32 @@
 int
 main(int argc, char *argv[])
 {
+  //if (mprotect(0x1337, 1) != -1) {
+  //  printf("ERROR: mprotect: no error on unaligned address\n");
+  //}
+
+  //if (mprotect(0, 1) != -1) {
+  //  printf("ERROR: mprotect: no error on unmapped address\n");
+  //}
+
   int* ptr = malloc(sizeof(int));
   printf("Unprotected read: %d\n", *ptr);
 
+  int pid;
+  if ((pid = fork()) == 0) {
+    printf("Unprotected read: %d\n", *ptr);
+    exit(0);
+  } else {
+    wait(0);
+  }
+
   //mprotect((void*) ptr, 1);
-  printf("UNREACHABLE: %d\n", *ptr);
+  if ((pid = fork()) == 0) {
+    printf("UNREACHABLE: %d\n", *ptr);
+    exit(1);
+  } else {
+    wait(0);
+    printf("UNREACHABLE: %d\n", *ptr);
+  }
   exit(1);
 }
